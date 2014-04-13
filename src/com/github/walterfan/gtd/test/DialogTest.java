c12d50b0aaa8c95f8bad91509415dcb3b78f672f
walterfan <walterfan@qq.com>
2014-04-13 22:08:27 +0800

    add test codes

--- src/com/github/walterfan/gtd/test/DialogTest.java
+++ src/com/github/walterfan/gtd/test/DialogTest.java
@@ -15,37 +15,19 @@ public class DialogTest extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.simple_linear);
-
-		/*
-		 * if (savedInstanceState == null) { // During initial setup, plug in
-		 * the details fragment. FragmentManager fragmentManager =
-		 * getFragmentManager(); FragmentTransaction fragmentTransaction =
-		 * fragmentManager.beginTransaction(); ConfirmDialogFragment dialogFrag
-		 * = new ConfirmDialogFragment();
-		 * dialogFrag.setArguments(getIntent().getExtras());
-		 * fragmentTransaction.add(R.id.blank_frame, dialogFrag);
-		 * fragmentTransaction.commit(); }
-		 */
 		setContentView(R.layout.simple_linear);
 
 		View tv = findViewById(R.id.textView1);
 
-		((TextView) tv)
-				.setText("Example of displaying an alert dialog with a DialogFragment");
+		((TextView) tv).setText("Example of displaying an alert dialog with a DialogFragment");
 
 		Button button = (Button) findViewById(R.id.button1);
 
 		button.setOnClickListener(new OnClickListener() {
-
 			public void onClick(View v) {
-
 				showConfirmDialog();
-
 			}
-
 		});
 
 	}
